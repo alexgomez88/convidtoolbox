017b6bf18c613f36aa9b8203932ac74261d2c71a
Alejandro Gomez <alejandrogomez88@gmail.com>
2020-03-30 09:05:24 -0400

    add some fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
 
 module.exports = {
   mode: "development",
@@ -55,5 +57,8 @@ module.exports = {
         encabezamiento: 'Aprendo Webpack en DesarrolloWeb.com',
       }
     }),
+    new CopyPlugin([
+      { from: 'src/res/', to: 'res/' , type:"dir"},
+    ], { copyUnmodified: true }),
   ]
 };

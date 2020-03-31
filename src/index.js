21d77624efda278d7a74fb1d9532a0606d76afe0
Alejandro Gomez <agomez@jidadesarrollos.com>
2020-03-31 00:26:23 -0400

    final fixes

--- src/index.js
+++ src/index.js
@@ -5,17 +5,19 @@ import App from './App.jsx';
 
 if (window.cordova) {
   document.addEventListener("deviceready", () => {
-    window.plugins.AdMob.setOptions({
-      publisherId: "ca-app-pub-7611014775630061/1946562091",  // Required
-      interstitialAdId: "ca-app-pub-7611014775630061/6544282657",  // Optional
-      //tappxIdiOS: "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
-      //tappxIdAndroid: "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
-      //tappxShare: 0.5                                        // Optional
+    admob.banner.config({
+      id: 'ca-app-pub-7611014775630061/1946562091',
+      isTesting: false,
+      autoShow: true
     });
+    admob.banner.prepare();
 
-    window.plugins.AdMob.createBannerView({
-      autoShowBanner: false
+    admob.interstitial.config({
+      id: 'ca-app-pub-7611014775630061/6544282657',
+      isTesting: false,
+      autoShow: true
     });
+    admob.interstitial.prepare();
 
     initApp();
   }, false);
@@ -25,4 +27,4 @@ if (window.cordova) {
 
 function initApp() {
   ReactDOM.render(<App />, document.getElementById('root'));
-};
+}

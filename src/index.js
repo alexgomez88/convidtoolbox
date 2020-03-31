dbd51b43f1fa583434a967338a93ab4ddaebe0b4
Alejandro Gomez <alejandrogomez88@gmail.com>
2020-03-30 22:03:33 -0400

    add ads

--- src/index.js
+++ src/index.js
@@ -5,6 +5,18 @@ import App from './App.jsx';
 
 if (window.cordova) {
   document.addEventListener("deviceready", () => {
+    window.plugins.AdMob.setOptions({
+      publisherId: "ca-app-pub-7611014775630061/1946562091",  // Required
+      interstitialAdId: "ca-app-pub-7611014775630061/6544282657",  // Optional
+      //tappxIdiOS: "/XXXXXXXXX/Pub-XXXX-iOS-IIII",            // Optional
+      //tappxIdAndroid: "/XXXXXXXXX/Pub-XXXX-Android-AAAA",        // Optional
+      //tappxShare: 0.5                                        // Optional
+    });
+
+    window.plugins.AdMob.createBannerView({
+      autoShowBanner: false
+    });
+
     initApp();
   }, false);
 } else {
